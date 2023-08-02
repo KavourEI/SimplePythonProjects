32620da0594c53f2c1453ee4be46d917f832a145
KavourEI <themis.kavour@icloud.com>
2023-08-03 01:01:52 +0300

    Guess_50US_States_Game

---
+++ Guess_the_states (US Edition)/main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import turtle
+
+df = pd.read_csv('50_states.csv', delimiter=',')
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    user_answer = screen.textinput(title=f"{len(guessed_states)}/50 Guess all the States",
+                                   prompt="What are the states' names?").title()
+    all_states = df.state.tolist()
+    if user_answer == 'Exit':
+        break
+    if user_answer in all_states and user_answer not in guessed_states:
+        guessed_states.append(user_answer)
+        t = turtle.Turtle()
+
+        t.hideturtle()
+        t.penup()
+        correct_data = df[df.state == user_answer]
+        t.goto(int(correct_data.x), int(correct_data.y))
+        t.write(user_answer)
+
+# States_missed:
+missed_ones = []
+for i in all_states:
+    if i not in guessed_states:
+        missed_ones.append(i)
+mos = pd.DataFrame(missed_ones)
+mos.to_csv("States_missed.csv")
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Include Several Simple Python Projects
 3. Snake Game, the one we all love from the old days. (+High Score, + High Score Saved)
 4. Pong game, fir two players. Simple yet fun!
 5. Turtle Crossing Road. Pass the turtle to the other side with patience!
+6. Guess The 50 US States Correctly! Give it a go. Can you find all 50 of them?
 
